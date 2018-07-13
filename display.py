a85e0875d95a36329b6d9961376f0d3413559f6f
OITtanigawa <e1b15064@oit.ac.jp>
2018-07-13 10:53:46 +0900

    display.py の更新

--- display.py
+++ display.py
@@ -2,22 +2,26 @@ import numpy
 import cv2
 
 flag = 0
-while(1):
-	#init img
-	img = None
-	
+while(1):	
 	#read text
 	text = open('test.txt')
 	img_num = text.read()
 	text.close()
-
-	#binding img
-	for i in range(int(img_num)-5,int(img_num)):
-		if i%5 == 1:
-			img = cv2.imread('cap'+str(i)+'.png')
-		else :
-			img = cv2.hconcat([img],[cv2.imread('cap'+str(i)+'.png')])
-	
+	if int(img_num) >5:
+		#binding img
+		for i in range(int(img_num)-5,int(img_num)):
+			if i%5 == 1:
+				img = cv2.imread('cap'+str(i)+'.png')
+			else :
+				img2 = imread('cap'+str(i)+'.png')
+				img = cv2.hconcat([img,img2])
+	else :
+		for i in range(int(img_num)-5,int(img_num)):
+			if i == 1:
+				img = cv2.imread('cap'+str(i)+'.png')
+			else :
+				img2 = imread('cap'+str(i)+'.png')
+				img = cv2.hconcat([img,img2])
 	#display img
 	cv2.namedWindow('display',cv2.WINDOW_NORMAL)
 	cv2.imshow('display',img)
